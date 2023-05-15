bd6f2facb39a4c1a6af7a99fa2131bf43fb59992
VITALIY1986 <staslego1986@gmail.com>
2023-05-15 16:33:21 +0300

    sdf

--- pages/checkout.js
+++ pages/checkout.js
@@ -6,8 +6,8 @@ import client from "../src/components/ApolloClient";
 const Checkout = ({data}) => (
 	<Layout>
 		<div className="checkout container mx-auto my-32 px-4 xl:px-0">
-		
-		
+			<h1 className="mb-5 text-2xl uppercase">Сторінка оформлення замовлення</h1>
+			<CheckoutForm countriesData={data?.wooCountries ?? {}}/>
 		</div>
 	</Layout>
 );
@@ -21,7 +21,7 @@ export async function getStaticProps() {
 
 	return {
 		props: {
-			
+			data: data || {}
 		},
 		revalidate: 1
 	};
