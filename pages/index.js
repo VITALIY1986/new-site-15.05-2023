5a867bf5dd18fa8b7325db07fd3e4c37bb888db9
VITALIY1986 <staslego1986@gmail.com>
2023-05-17 15:10:34 +0300

    sdfsdfs

--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,7 @@ export default function Home (props) {
 		
 			</div>
 					{/*Post
-					<div className="products container mx-auto my-32 px-4 ">
+					<div className="products container  mx-auto my-32 px-4 ">
 					<h2 className="products-main-title main-title mb-5 text-3xl text-center uppercase"><span className="main-title-inner">{postsCategory?.name}</span></h2>
 					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
 						{ posts.length ? (
