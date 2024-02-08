2faef0b11fd7ef3a87fcdb2fb1171a2e0f3be3e5
VITALIY1986 <staslego1986@gmail.com>
2024-02-08 17:24:00 +0200

    asdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
       './src/components/**/*.js',
       './pages/**/*.js'],
     theme: {
+      fontSize: {
+        sm: '0.6rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
       extend: {
         height: {
           'almost-screen': 'calc(-16rem + 100vh)',
