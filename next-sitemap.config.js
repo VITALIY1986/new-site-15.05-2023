59c53149267a4aa1d388a9fd5d8d73152cb98729
VITALIY1986 <staslego1986@gmail.com>
2024-03-21 01:22:08 +0200

    asddsasdads

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC || 'http://localhost:3000/',
+    siteUrl: process.env.NEXT_PUBLIC || 'https://lux-tex.com.ua/',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
