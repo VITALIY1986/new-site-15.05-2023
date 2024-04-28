7b28595e417a17da23fa35575b016bca9b546722
VITALIY1986 <staslego1986@gmail.com>
2024-04-28 13:57:25 +0300

    fgdhdhd

---
+++ pages/checkout.js
@@ -0,0 +1,18 @@
+import Layout from "../src/components/Layout";
+import CheckoutForm from "../src/components/checkout/CheckoutForm";
+import GET_COUNTRIES from "../src/queries/get-countries";
+import client from "../src/components/ApolloClient";
+
+const Checkout = ({data}) => (
+	<Layout>
+		<div className="checkout container mx-auto my-32 px-4 xl:px-0">
+			<h1 className="mb-5 text-2xl uppercase">Сторінка оформлення замовлення</h1>
+			<CheckoutForm />
+		</div>
+	</Layout>
+);
+
+export default Checkout;
+
+
+
