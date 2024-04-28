e5dc4be4f1243f0cf053c8fc61417d55c83bdead
VITALIY1986 <staslego1986@gmail.com>
2024-04-28 13:51:54 +0300

    gjgjgj

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -28,7 +28,7 @@ const Accordion = (props) => {
 
   const renderedQuestionsAnswers = questionsAnswers.map((item, index) => {
     const showDescription = index === activeIndex ? "show-descriptionn" : "";
-    const fontWeightBold = index === activeIndex ? "text-red-300 font-weight-bold " : "";
+    const fontWeightBold = index === activeIndex ? " font-weight-bold " : "";
     
     const ariaExpanded = index === activeIndex ? "true" : "false";
     return (
