4382187cb7dbefb825da222d60cc1feb78e14403
VITALIY1986 <staslego1986@gmail.com>
2024-05-20 11:53:42 +0300

    sggsjhfjfkjfk

--- pages/index.js
+++ pages/index.js
@@ -174,8 +174,7 @@ export default function Home (props) {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
             {featuredproducts.length ? (
 				
-			
-                featuredproducts.map(product => <ProductHome key={product.id} isLoggedIn={isLoggedIn} product={product}/>)
+			featuredproducts.map(product => <ProductHome key={product.id} isLoggedIn={isLoggedIn} product={product}/>)
             ) : ''}
         </div>
     </div>
